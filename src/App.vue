134aecd6b1dd07aac8a3198be3489ad2b922281d
Gilad Dagan <giladagan53@gmail.com>
2023-02-12 14:01:04 +0200

    Basic template

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script>
+import AppHeader from '@/cmps/app-header.vue'
+import AppFooter from '@/cmps/app-footer.vue'
+export default {
+    components: {
+        AppHeader,
+        AppFooter,
+    },
+}
+</script>
+
+<template>
+  <AppHeader />
+  <RouterView />
+  <AppFooter />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/cmps/app-header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <span>Contacts</span>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/contact">Contacts</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {}
+</script>
