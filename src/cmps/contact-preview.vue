38a91e4f9bee872a73a6f6a589d876753610a17c
Gilad Dagan <giladagan53@gmail.com>
2023-02-12 15:16:20 +0200

    List & Preview

---
+++ src/cmps/contact-preview.vue
@@ -0,0 +1,24 @@
+<template>
+    <article class="contact-preview">
+        <h3>{{ contact.name }}</h3>
+        <img class="robot" :src="'https://robohash.org/' + contact.name + '?set=set5'" alt="">
+        <p>{{ contact.email }}</p>
+        <p>{{ contact.phone }}</p>
+
+    </article>
+</template>
+
+<script>
+export default {
+    props: {
+        contact: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/contact-index.vue
+++ src/views/contact-index.vue
@@ -1,9 +1,40 @@
 <template>
-
+    <div class="main-contai">
+        <ContactList @remove="removeContact" v-if="contacts" :contacts="contacts" />
+    </div>
 </template>
 
 <script>
+import ContactList from '../cmps/contact-list.vue'
+import {contactService} from '../services/contact-service'
+
 export default {
+    data() {
+        return {
+            contacts: null,
+        }
+    },
+    async created() {
+        this.contacts = await contactService.getContacts()
+    },
+    methods: {
+        async removeCar(contactId) {
+            // const msg = {
+            //     txt: `Car ${carId} deleted.`,
+            //     type: 'success',
+            //     timeout: 2500,
+            // }
+            await contactService.deleteContact(contactId)
+            this.contacts = this.contacts.filter(contact => car._id !== contactId)
+            // eventBus.emit('user-msg', msg)
+        },
+        // onSetFilterBy(filterBy) {
+        //     this.filterBy = filterBy
+        // },
+    },
+    components: {
+        ContactList
+    }
 
 }
 </script>
